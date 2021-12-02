76dfe1ee0f6396a8c1569edb8aa6ed06f3fef318
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2021-12-02 20:39:10 +0300

    add

---
+++ PS/id1149.ps1
@@ -0,0 +1,8 @@
+﻿$Date = (Get-Date).AddDays(-7)
+Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\7days.html
+$Date = (Get-Date).AddDays(-1)
+Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\1days.html
+$Date = (Get-Date).AddDays(-14)
+Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\14days.html
+$Date = (Get-Date).AddDays(-30)
+Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\30days.html
---
+++ PS/id1307.ps1
@@ -0,0 +1,8 @@
+﻿$Date = (Get-Date).AddDays(-30)
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-SessionBroker-Client/Operational";id='1307';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\30_1307days.html
+$Date = (Get-Date).AddDays(-1)
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-SessionBroker-Client/Operational";id='1307';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\1_1307days.html
+$Date = (Get-Date).AddDays(-7)
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-SessionBroker-Client/Operational";id='1307';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\7_1307days.html
+$Date = (Get-Date).AddDays(-14)
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-SessionBroker-Client/Operational";id='1307';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\14_1307days.html
