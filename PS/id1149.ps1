cfda016afa371c47c26998572477e67ec87127be
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2021-12-03 13:46:42 +0300

    update

--- PS/id1149.ps1
+++ PS/id1149.ps1
@@ -1,4 +1,10 @@
-﻿$Date = (Get-Date).AddDays(-7)
+﻿###
+* Берет текущее время ($data) и выбирает диапозон дней (AddDays(-7))
+
+* Идет в event-log microsoft-windows-terminalservices-remoteconnectionmanager/operational, выбирает ID=1149 и конвертирует в хтпл.
+
+###
+$Date = (Get-Date).AddDays(-7)
 Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\7days.html
 $Date = (Get-Date).AddDays(-1)
 Get-WinEvent -FilterHashtable @{logname="microsoft-windows-terminalservices-remoteconnectionmanager/operational";id='1149';StartTime=$Date}  | ConvertTo-Html -Property Message, id, TimeCreated -Head $Header | Out-File -FilePath C:\ps\1days.html
